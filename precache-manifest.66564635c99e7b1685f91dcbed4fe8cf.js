f971b68a7dd9be72211c6a7d24b53a31f2a5e123
Alexandr Katrazhenko <katrazhenko@semalt.com>
2021-02-06 10:12:43 +0200

    Updates

---
+++ precache-manifest.66564635c99e7b1685f91dcbed4fe8cf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4ff038e4f99ea0ce109f5847efcee9",
+    "url": "/tgxkat/index.html"
+  },
+  {
+    "revision": "d087e4fb1c0a6b4669b6",
+    "url": "/tgxkat/static/css/0.9b2f7aac.chunk.css"
+  },
+  {
+    "revision": "573416d283ef7775fbdc",
+    "url": "/tgxkat/static/css/3.c0b0cb9e.chunk.css"
+  },
+  {
+    "revision": "280ad9af008a32a35b87",
+    "url": "/tgxkat/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "7ca94f74868fc9115764",
+    "url": "/tgxkat/static/css/6.bf54a11b.chunk.css"
+  },
+  {
+    "revision": "8c9742fa6357275f8e88",
+    "url": "/tgxkat/static/css/main.705dca1f.chunk.css"
+  },
+  {
+    "revision": "d087e4fb1c0a6b4669b6",
+    "url": "/tgxkat/static/js/0.2998a8b4.chunk.js"
+  },
+  {
+    "revision": "573416d283ef7775fbdc",
+    "url": "/tgxkat/static/js/3.8b564064.chunk.js"
+  },
+  {
+    "revision": "2353aeae9d79179c3d86",
+    "url": "/tgxkat/static/js/4.90262976.chunk.js"
+  },
+  {
+    "revision": "63c28f3460550eadf6199955a8ca9c59",
+    "url": "/tgxkat/static/js/4.90262976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280ad9af008a32a35b87",
+    "url": "/tgxkat/static/js/5.91dcc819.chunk.js"
+  },
+  {
+    "revision": "94d345d07f28e397ce0d275d9e7db62d",
+    "url": "/tgxkat/static/js/5.91dcc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca94f74868fc9115764",
+    "url": "/tgxkat/static/js/6.d7030b61.chunk.js"
+  },
+  {
+    "revision": "eab2bcaac0afde6cee10",
+    "url": "/tgxkat/static/js/7.2f309bcc.chunk.js"
+  },
+  {
+    "revision": "8c9742fa6357275f8e88",
+    "url": "/tgxkat/static/js/main.79aa0b0a.chunk.js"
+  },
+  {
+    "revision": "74826ae049a6aa5a9870",
+    "url": "/tgxkat/static/js/runtime-main.0822ed95.js"
+  },
+  {
+    "revision": "c6a92d89aa71027ea621d52bb03ad681",
+    "url": "/tgxkat/static/media/bubble-tail-left.c6a92d89.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/tgxkat/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/tgxkat/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "8e3f975b0dac0d7ddc9f5ab85f24d15a",
+    "url": "/tgxkat/static/media/telegram-logo.8e3f975b.svg"
+  }
+]);
